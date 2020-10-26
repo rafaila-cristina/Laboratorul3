690d8f1d14f5e816a52aa618692f494303246f1a
rafaila-cristina <rafaila.cristina@gmail.com>
2020-10-26 09:40:20 +0200

    Modificare clasa biblioteca
    redenumire membru, add metoda remove
    
    Signed-off-by: rafaila-cristina <rafaila.cristina@gmail.com>

--- src/ro/mta/facc/softing/Biblioteca.java
+++ src/ro/mta/facc/softing/Biblioteca.java
@@ -1,41 +1,46 @@
 package ro.mta.facc.softing;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 public class Biblioteca {
-    private ArrayList<Carte> carti;
+    private ArrayList<Carte> listaCarti;
 
     public void sort()
     {
-        Collections.sort(carti);
+        Collections.sort(listaCarti);
     }
 
 
-    public Biblioteca(ArrayList<Carte> carti) {
-        carti=new ArrayList<Carte>();
-        for (Carte c:carti)
+    public Biblioteca(ArrayList<Carte> listaCarti) {
+        this.listaCarti=new ArrayList<Carte>();
+        for (Carte c:listaCarti)
         {
-            this.carti.add(c);
+            this.listaCarti.add(c);
         }
     }
 
     public Biblioteca() {
-        carti=new ArrayList<Carte>();
+        listaCarti =new ArrayList<Carte>();
     }
 
 
+
+    public void removeBook(Carte c)
+    {
+        listaCarti.remove(c);
+    }
+
     public void add(Carte c)
     {
-        this.carti.add(c);
+        this.listaCarti.add(c);
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
         builder.append("Biblioteca contine: \n");
-        for (Carte c:carti)
+        for (Carte c: listaCarti)
         {
             builder.append(c.toString());
             builder.append("\n");
